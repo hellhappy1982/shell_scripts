1a901efc9ea0ce1f2c1f064618f8c3b2ac336488
hellhappy <hellhappy1982@gmail.com>
2017-05-29 00:35:01 +0800

    .

--- web-7,8/nginx_conf.sh
+++ web-7,8/nginx_conf.sh
@@ -22,8 +22,7 @@ http {
 }">/application/nginx/conf/nginx.conf
 
 #www_bbs_blog_conf
-mkdir -p /application/nginx/conf/extra/ /application/nginx/html/www bbs
-mkdir -p /application/nginx/html/blog
+mkdir -p /application/nginx/conf/extra/ /application/nginx/html/{www,blog,bbs}
 echo "server {
     listen       80;
     server_name  blog.etiantian.org;
@@ -40,7 +39,6 @@ echo "server {
 } ">/application/nginx/conf/extra/blog.conf
 cd /home/oldboy/tools && tar xf wordpress-4.7.3-zh_CN.tar.gz 
 mv wordpress/* /application/nginx/html/blog/
-chown -R www.www /application/nginx/html/blog/
 
 echo "server {
     listen       80;
@@ -48,7 +46,7 @@ echo "server {
     location / {
         root   html/www;
         index index.php  index.html index.htm;
-    location ~* .*\.(php|php5)?$ {
+    location ~* .*\\.(php|php5)?$ {
         root html/www;
         fastcgi_pass  127.0.0.1:9000;
         fastcgi_index index.php;
@@ -57,7 +55,6 @@ echo "server {
     }
 }">/application/nginx/conf/extra/www.conf
 cd /home/oldboy/tools && tar xf DedeCmsV5.6-UTF8-Final.tar.gz -C /application/nginx/html/www/
-chown -R www.www /application/nginx/html/www/
 
 echo "server {
     listen       80;
@@ -74,10 +71,23 @@ echo "server {
     }
 }">/application/nginx/conf/extra/bbs.conf
 unzip Discuz_X3.3_SC_UTF8.zip -d /application/nginx/html/bbs/
-chown -R www.www /application/nginx/html/bbs/
+chown -R www.www /application/nginx/html/
+
+#start_nginx_php
 /application/nginx/sbin/nginx 
 /application/php/sbin/php-fpm
 /application/nginx/sbin/nginx -s reload
+
+#blog_mysql_conf
+cd /application/nginx/html/blog/
+\cp wp-config-sample.php wp-config.php
+sed -i s#database_name_here#wordpress#g /application/nginx/html/blog/wp-config.php
+sed -i s#username_here#wordpress#g /application/nginx/html/blog/wp-config.php
+sed -i s#password_here#oldboy123#g /application/nginx/html/blog/wp-config.php
+sed -i s#localhost#172.16.1.51#g /application/nginx/html/blog/wp-config.php
+
+echo "10.0.0.7 www.etiantian.org bbs.etiantian.org blog.etiantian.org
+10.0.0.8 www.etiantian.org bbs.etiantian.org blog.etiantian.org">>/etc/hosts
 
 
 
